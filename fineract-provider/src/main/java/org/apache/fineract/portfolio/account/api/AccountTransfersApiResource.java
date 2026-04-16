5ad3faf6023cc97aa0e79302b9dd74d025df3b3c
Anhelina <angelinazmitrevic2@gmail.com>
2026-04-16 19:24:45 +0300

    Update AccountTransfersApiResource.java

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/account/api/AccountTransfersApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/account/api/AccountTransfersApiResource.java
@@ -63,7 +63,7 @@ public class AccountTransfersApiResource {
     @GET
     @Path("template")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Retrieve Account Transfer Template", operationId = "retrieveTemplateAccountTransfer", description = "This is a convenience resource. It can be useful when building maintenance user interface screens for client applications. The template data returned consists of any or all of:\n\n"
+    @Operation(summary = "Retrieve Account Transfer Template", operationId = "retrieveTemplateAccountTransfer", tags = {"Account Transfers"}, description = "This is a convenience resource. It can be useful when building maintenance user interface screens for client applications. The template data returned consists of any or all of:\n\n"
             + "\n\n" + "Field Defaults\n\n" + "Allowed Value Lists\n\n" + "Example Requests:\n\n" + "\n\n"
             + "accounttransfers/template?fromAccountType=2&fromOfficeId=1\n\n" + "\n\n"
             + "accounttransfers/template?fromAccountType=2&fromOfficeId=1&fromClientId=1\n\n" + "\n\n"
@@ -83,7 +83,7 @@ public class AccountTransfersApiResource {
     @POST
     @Consumes({ MediaType.APPLICATION_JSON })
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Create new Transfer", operationId = "createAccountTransfer", description = "Ability to create new transfer of monetary funds from one account to another.")
+    @Operation(summary = "Create new Transfer", operationId = "createAccountTransfer", tags = {"Account Transfers"}, description = "Ability to create new transfer of monetary funds from one account to another.")
     @RequestBody(required = true, content = @Content(schema = @Schema(implementation = AccountTransferRequest.class)))
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AccountTransfersApiResourceSwagger.PostAccountTransfersResponse.class)))
     public CommandProcessingResult create(@Parameter(hidden = true) AccountTransferRequest accountTransferRequest) {
@@ -95,7 +95,7 @@ public class AccountTransfersApiResource {
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "List account transfers", operationId = "retrieveAllAccountTransfers", description = "Lists account's transfers\n\n"
+    @Operation(summary = "List account transfers", operationId = "retrieveAllAccountTransfers", tags = {"Account Transfers"}, description = "Lists account's transfers\n\n"
             + "Example Requests:\n\n" + "\n\n" + "accounttransfers")
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AccountTransfersApiResourceSwagger.GetAccountTransfersResponse.class)))
     public Page<AccountTransferData> retrieveAll(@QueryParam("externalId") @Parameter(description = "externalId") final String externalId,
@@ -116,7 +116,7 @@ public class AccountTransfersApiResource {
     @GET
     @Path("{transferId}")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Retrieve account transfer", operationId = "retrieveOneAccountTransfer", description = "Retrieves account transfer\n\n"
+    @Operation(summary = "Retrieve account transfer", operationId = "retrieveOneAccountTransfer", tags = {"Account Transfers"}, description = "Retrieves account transfer\n\n"
             + "Example Requests :\n\n" + "\n\n" + "accounttransfers/1")
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AccountTransfersApiResourceSwagger.GetAccountTransfersResponse.GetAccountTransfersPageItems.class)))
     public AccountTransferData retrieveOne(@PathParam("transferId") @Parameter(description = "transferId") final Long transferId) {
@@ -127,7 +127,7 @@ public class AccountTransfersApiResource {
     @GET
     @Path("templateRefundByTransfer")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Retrieve Refund of an Active Loan by Transfer Template", operationId = "retrieveTemplateRefundByTransfer", description = "Retrieves Refund of an Active Loan by Transfer Template"
+    @Operation(summary = "Retrieve Refund of an Active Loan by Transfer Template", operationId = "retrieveTemplateRefundByTransfer", tags = {"Account Transfers"}, description = "Retrieves Refund of an Active Loan by Transfer Template"
             + "Example Requests :\n\n" + "\n\n"
             + "accounttransfers/templateRefundByTransfer?fromAccountId=2&fromAccountType=1& toAccountId=1&toAccountType=2&toClientId=1&toOfficeId=1")
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AccountTransfersApiResourceSwagger.GetAccountTransfersTemplateRefundByTransferResponse.class)))
@@ -144,7 +144,7 @@ public class AccountTransfersApiResource {
     @Path("refundByTransfer")
     @Consumes({ MediaType.APPLICATION_JSON })
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Refund of an Active Loan by Transfer", operationId = "refundByTransfer", description = "Ability to refund an active loan by transferring to a savings account.")
+    @Operation(summary = "Refund of an Active Loan by Transfer", operationId = "refundByTransfer", tags = {"Account Transfers"}, description = "Ability to refund an active loan by transferring to a savings account.")
     @RequestBody(required = true, content = @Content(schema = @Schema(implementation = AccountTransferRequest.class)))
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = AccountTransfersApiResourceSwagger.PostAccountTransfersRefundByTransferResponse.class)))
     public CommandProcessingResult templateRefundByTransferPost(@Parameter(hidden = true) AccountTransferRequest accountTransferRequest) {
