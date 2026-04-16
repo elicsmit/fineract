f662d43cc6fcedf05410d86b14374fe2c82077cb
Anhelina <angelinazmitrevic2@gmail.com>
2026-04-16 19:59:51 +0300

    Update LoanCollateralManagementApiResource.java

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/collateralmanagement/api/LoanCollateralManagementApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/collateralmanagement/api/LoanCollateralManagementApiResource.java
@@ -48,7 +48,7 @@ public class LoanCollateralManagementApiResource {
     @DELETE
     @Path("{id}")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(description = "Delete Loan Collateral", summary = "Delete Loan Collateral")
+    @Operation(description = "Delete Loan Collateral", tags = {"Loan Collateral Management"}, summary = "Delete Loan Collateral")
     public CommandProcessingResult deleteLoanCollateral(@PathParam("loanId") @Parameter(description = "loanId") final Long loanId,
             @PathParam("id") @Parameter(description = "loan collateral id") final Long id) {
 
@@ -59,7 +59,7 @@ public class LoanCollateralManagementApiResource {
     @GET
     @Path("{collateralId}")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(description = "Get Loan Collateral Details", summary = "Get Loan Collateral Details")
+    @Operation(description = "Get Loan Collateral Details", tags = {"Loan Collateral Management"}, summary = "Get Loan Collateral Details")
     public LoanCollateralResponseData getLoanCollateral(
             @PathParam("collateralId") @Parameter(description = "collateralId") final Long collateralId) {
         return this.loanCollateralManagementReadPlatformService.getLoanCollateralResponseData(collateralId);
