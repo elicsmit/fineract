c76371455f01c94a2439dd9f76ea5f7393ae93b6
Anhelina <angelinazmitrevic2@gmail.com>
2026-04-16 19:58:04 +0300

    Update ClientCollateralManagementApiResource.java

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/collateralmanagement/api/ClientCollateralManagementApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/collateralmanagement/api/ClientCollateralManagementApiResource.java
@@ -66,7 +66,7 @@ public class ClientCollateralManagementApiResource {
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Get Clients Collateral Products", operationId = "getClientCollateralProducts", description = "Get Collateral Product of a Client")
+    @Operation(summary = "Get Clients Collateral Products", operationId = "getClientCollateralProducts", tags = {"Client Collateral Management"}, description = "Get Collateral Product of a Client")
     public List<ClientCollateralManagementData> getClientCollateral(
             @PathParam("clientId") @Parameter(description = "clientId") final Long clientId, @Context final UriInfo uriInfo,
             @QueryParam("prodId") @Parameter(description = "prodId") final Long prodId) {
@@ -80,7 +80,7 @@ public class ClientCollateralManagementApiResource {
     @GET
     @Path("{clientCollateralId}")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Get Client Collateral Data", operationId = "getClientCollateralData", description = "Get Client Collateral Data")
+    @Operation(summary = "Get Client Collateral Data", operationId = "getClientCollateralData", tags = {"Client Collateral Management"}, description = "Get Client Collateral Data")
     public ClientCollateralManagementData getClientCollateralData(
             @PathParam("clientId") @Parameter(description = "clientId") final Long clientId,
             @PathParam("clientCollateralId") @Parameter(description = "clientCollateralId") final Long collateralId) {
@@ -94,7 +94,7 @@ public class ClientCollateralManagementApiResource {
     @GET
     @Path("template")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Get Client Collateral Template", operationId = "getClientCollateralTemplate", description = "Get Client Collateral Template")
+    @Operation(summary = "Get Client Collateral Template", operationId = "getClientCollateralTemplate", tags = {"Client Collateral Management"}, description = "Get Client Collateral Template")
     public List<LoanCollateralTemplateData> getClientCollateralTemplate(
             @PathParam("clientId") @Parameter(description = "clientId") final Long clientId) {
         return this.clientCollateralManagementReadPlatformService.getLoanCollateralTemplate(clientId);
@@ -103,7 +103,7 @@ public class ClientCollateralManagementApiResource {
     @POST
     @Produces({ MediaType.APPLICATION_JSON })
     @Consumes({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Add New Collateral For a Client", operationId = "addClientCollateral", description = "Add New Collateral For a Client")
+    @Operation(summary = "Add New Collateral For a Client", operationId = "addClientCollateral", tags = {"Client Collateral Management"}, description = "Add New Collateral For a Client")
     @RequestBody(required = true, content = @Content(schema = @Schema(implementation = ClientCollateralRequest.class)))
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ClientCollateralManagementApiResourceSwagger.PostClientCollateralResponse.class)))
     public CommandProcessingResult addCollateral(@PathParam("clientId") @Parameter(description = "clientId") final Long clientId,
@@ -118,7 +118,7 @@ public class ClientCollateralManagementApiResource {
     @Path("{collateralId}")
     @Produces({ MediaType.APPLICATION_JSON })
     @Consumes({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Update New Collateral of a Client", operationId = "updateClientCollateral", description = "Update New Collateral of a Client")
+    @Operation(summary = "Update New Collateral of a Client", operationId = "updateClientCollateral", tags = {"Client Collateral Management"}, description = "Update New Collateral of a Client")
     @RequestBody(required = true, content = @Content(schema = @Schema(implementation = UpdateClientCollateralRequest.class)))
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ClientCollateralManagementApiResourceSwagger.PutClientCollateralResponse.class)))
     public CommandProcessingResult updateCollateral(@PathParam("clientId") @Parameter(description = "clientId") final Long clientId,
@@ -134,7 +134,7 @@ public class ClientCollateralManagementApiResource {
     @DELETE
     @Path("{collateralId}")
     @Produces({ MediaType.APPLICATION_JSON })
-    @Operation(summary = "Delete Client Collateral", operationId = "deleteClientCollateral", description = "Delete Client Collateral")
+    @Operation(summary = "Delete Client Collateral", operationId = "deleteClientCollateral", tags = {"Client Collateral Management"}, description = "Delete Client Collateral")
 
     @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ClientCollateralManagementApiResourceSwagger.DeleteClientCollateralResponse.class)))
     public CommandProcessingResult deleteCollateral(@PathParam("clientId") @Parameter(description = "clientId") final Long clientId,
